7df8f9b5dfe2c59336f65151632406478cabe1c4
rascasse-monaco <mktwkhr@gmail.com>
2020-12-17 17:05:19 +0900

    フォームの追加など

--- countdown.js
+++ countdown.js
@@ -1,19 +1,36 @@
 'use strict'
 
+const clock = {
+  hour: 0,
+  min: 0,
+  sec: 0,
+  milsec: 0,
+};
+
+clock.hour = document.getElementById('hour').Value;
+clock.min = document.getElementById('min').value;
+clock.sec = document.getElementById('sec').value
+
 function culcMillisecond(hour, min, sec) {
   const inputNum = {
     sec: sec,
     min: min * 60,
     hour: hour * 60 * 60
   };
-  return (inputNum.sec + inputNum.min + inputNum.hour) * 1000
+  return (inputNum.sec + inputNum.min + inputNum.hour)
 }
 
-const min = Math.floor(culcMillisecond(0, 5, 30) / 60 / 1000);
-const sec = culcMillisecond(0, 5, 30) % (60 * 1000) / 1000;
+clock.milsec = culcMillisecond(0, 5, 30);
 
-function countDown() {
-  document.getElementById('countdown').innerText =`func.milsec ${culcMillisecond(0, 5, 30)} = ${min}分${sec}秒`;
+const culcTime = {
+  min: Math.floor(clock.milsec / 60),
+  sec: clock.milsec % 60
 }
 
-countDown();
+setInterval(() => {
+  document.getElementById('milsec').innerText = `sec=${clock.milsec} / ${culcTime.min}分${culcTime.sec}秒`;
+  clock.milsec --;
+  culcTime.min = Math.floor(clock.milsec / 60);
+  culcTime.sec = clock.milsec % 60;
+}, 1000)
+
