7a36e54710c49ed29658182ac3cddc8677d80845
rascasse-monaco <mktwkhr@gmail.com>
2020-12-28 17:13:08 +0900

    ボタン動作の変更とスイッチ用変数の削除

--- countdown.js
+++ countdown.js
@@ -1,7 +1,6 @@
 'use strict'
 
 let countNum = 0;
-let switchTrgNum = 0;// ボタン制御用変数、0の時set可能、1の時start、2の時pause、それぞれ可能
 let interVal = null;// setInterval代入用
 
 //フォームから時間を取得、数値に変換しcountNumに代入
@@ -20,14 +19,12 @@ function getTime() {
 //時間設定&ボタン置き換え実行用関数
 function setTimer() {  
   getTime();
-  if (switchTrgNum === 0 && countNum > 0) {
-    switchTrgNum = 1;  
+  if (countNum > 0) {
     removeAllChildren('settingArea');
     removeAllChildren('buttonArea');
     createBtn('settingArea', 'start', 'start()', '<i class="fas fa-play"></i> Start');  
     document.getElementById('timeArea').innerText =
     `${toDoubleDigits(culcToTimeDisplay(countNum).hour)}:${toDoubleDigits(culcToTimeDisplay (countNum).min)}:${toDoubleDigits(culcToTimeDisplay(countNum).sec)}`;
-    replaceSetButton();
     }
 }
 /**
@@ -53,32 +50,32 @@ function culcToTimeDisplay(Num) {
 }
 //カウントダウン実行関数
 function start() {
-  if (switchTrgNum === 1) {
-    switchTrgNum = 2;
+    removeAllChildren('settingArea');
+    replaceSetButton();
     interVal = setInterval(() => {
       countNum --;
       document.getElementById('timeArea').innerText =
       `${toDoubleDigits(culcToTimeDisplay(countNum).hour)}:${toDoubleDigits(culcToTimeDisplay(countNum).min)}:${toDoubleDigits  (culcToTimeDisplay(countNum).sec)}`;
         if (countNum === 0) {
         removeAllChildren('settingArea');
-        createBtn('settingArea', 'alarmStop', 'mute()', '<i class="fas fa-volume-mute"></i> Alarm Stop')
-        removeAllChildren('buttonArea');
-        createBtn('buttonArea', 'set', 'reload()', '<i class="fas fa-clock"></i> Set Timer Again')
+        createBtn('settingArea', 'alarmStop', 'mute()', '<i class="fas fa-volume-mute fa-lg"></i> Sound Stop');
+        createBtn('settingArea', 'reset', 'reload()', '<i class="fas fa-redo"></i> Reset');  
         alarm();//アラーム音
-        pause();
+        stopTimer();
       }
     }, 1000);
-  }
+  
 }
 //タイマー一時停止関数
 function pause() {
-  if (switchTrgNum === 2 && countNum > 0) {
-    switchTrgNum = 1;
     clearInterval(interVal);
-  } else {
-    switchTrgNum = 0;
-    clearInterval(interVal);
-  }
+    removeAllChildren('settingArea');
+    createBtn('settingArea', 'startShort', 'start()', '<i class="fas fa-play"></i> Re Start');
+    createBtn('settingArea', 'reset', 'reload()', '<i class="fas fa-redo"></i> Reset');  
+}
+//タイマー停止関数
+function stopTimer() {
+  clearInterval(interVal);
 }
 //リセット、再読み込みする。
 function reload() {
@@ -86,7 +83,7 @@ function reload() {
 }
 //セットボタンをおしたら表示を変更する。
 function replaceSetButton() {
-  const buttonAreaID = document.getElementById('buttonArea');
+  const settingAreaID = document.getElementById('settingArea');
   //Pauseを表示。
   const pauseButton = document.createElement('button');
         pauseButton.setAttribute('type', 'button');
@@ -101,9 +98,9 @@ function replaceSetButton() {
         resetButton.setAttribute('onclick', 'reload()');
         resetButton.setAttribute('value', 'Reset');
 
-  buttonAreaID.appendChild(pauseButton)
+  settingAreaID.appendChild(pauseButton)
   pauseButton.innerHTML = '<i class="fas fa-pause"></i> Pause'
-  buttonAreaID.appendChild(resetButton);
+  settingAreaID.appendChild(resetButton);
   resetButton.innerHTML = '<i class="fas fa-redo"></i> Reset'
 }
 /**
@@ -112,12 +109,24 @@ function replaceSetButton() {
  */
 function removeAllChildren(id) {
   const element = document.getElementById(`${id}`);
+
   while (element.firstChild) {
     element.removeChild(element.firstChild);
   }
 }
 /**
- * 子要素をまとめて削除する
+ * 子要素を削除する
+ * @param {String} parentID 'parentID'親要素のID
+ * @param {String} childID 'childID'子要素のID
+ */
+function removeChild(parentID, childID) {
+  const elementParentID = document.getElementById(`${parentID}`);
+  const elementChildID = document.getElementById(`${childID}`)
+
+  elementParentID.removeChild(elementChildID);
+}
+/**
+ * 子要素のボタンを作成する関数
  * @param {String} parentAreaID 'parentAreaID' 親要素のID
  * @param {String} id 'id' ボタンのID
  * @param {String} funcName 'funcName' ボタンが呼びたしたい関数名
@@ -162,7 +171,3 @@ function mute() {
   audio.pause();
 }
 
-function switchNum(num) {
-  const switchNum = num;
-  return switchNum;
-}
